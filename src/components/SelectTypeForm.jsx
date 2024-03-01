a0faf7d6356c6f0b8df9ba99f558f2e20319d8c7
hanna <hannaphamm1007@gmail.com>
2024-03-01 11:10:59 +0100

    completed

--- src/components/SelectTypeForm.jsx
+++ src/components/SelectTypeForm.jsx
@@ -1,10 +1,14 @@
-function SelectTypeForm(props) {
+function SelectTypeForm({setDataType}) {
   // Write code here...
+  
+  const handleSelectChange = (event) => {
+  setDataType(event.target.value);
+  };
 
   return (
     <form className="three-column-grid__expand-two gap-md">
       <label htmlFor="type">Data Type</label>
-      <select id="type" name="type">
+      <select id="type" name="type" onChange={handleSelectChange}>
         <option value="">Please select...</option>
         <option value="people">People</option>
         <option value="planets">Planets</option>
